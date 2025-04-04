0d27d931a3b4724b87f7bbafcac1593526966b22
Michal Fujarewicz <michal.fujarewicz@gmail.com>
2025-04-04 18:11:02 +0200

    basic implementation of add match and get summary

--- src/main/java/org/scoreboard/Scoreboard.java
+++ src/main/java/org/scoreboard/Scoreboard.java
@@ -1,14 +1,24 @@
 package org.scoreboard;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Scoreboard {
-    public void startMatch(String homeTeam, String awayTeam) {
+    ArrayList<Match> matches;
 
+    public Scoreboard() {
+        matches = new ArrayList<Match>();
+    }
+
+
+    public void startMatch(String homeTeam, String awayTeam) {
+        matches.add(
+                new Match(homeTeam, awayTeam, 0, 0)
+        );
     }
 
     public List<Match> getSummary() {
-        return null;
+        return matches;
     }
 }
 
